599feed979917bd97fc4895b86d18e5a981e65b3
DESKTOP-PILHKCN\mitik <mitikusel@gmail.com>
2021-09-30 20:15:03 -0700

    Added for the first time

--- src/main/java/com/example/reactiveexamples/ReactiveExamplesApplication.java
+++ src/main/java/com/example/reactiveexamples/ReactiveExamplesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactiveExamplesApplication {
 
+    //Added to test commit
     public static void main(String[] args) {
         SpringApplication.run(ReactiveExamplesApplication.class, args);
     }
